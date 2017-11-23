132404310754ea544a497e38df7b08a8e0410b2a
Mikhail Novikov <mixail.novikov@gmail.com>
2017-11-23 13:41:52 +0300

    fix styles

--- src/components/AppLogo/index.js
+++ src/components/AppLogo/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { string } from 'prop-types';
+import c from 'classnames';
 
 import logoPath from './logo.png';
 import './style.css';
 
 class AppLogo extends Component {
+  static propTypes = {
+    className: string,
+  }
+
   render() {
     return (
-      <div className="AppLogo">
+      <div className={c('AppLogo', this.props.className)}>
         <img className="AppLogo-image" src={logoPath} />
         <div className="AppLogo-text">Books</div>
       </div>
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { string } from 'prop-types';
+import c from 'classnames';
+
 import searchIcon from './search.png';
 import './style.css';
 
 class SearchForm extends Component {
+  static propTypes = {
+    className: string,
+  };
+
   render() {
     return (
-      <form className="SearchForm" onSubmit={this.handleSubmit}>
-        <input className="SearchForm__input" autoFocus type="text" size="66" />
+      <form className={c('SearchForm', this.props.className)} onSubmit={this.handleSubmit}>
+        <input className="SearchForm__input" autoFocus type="text" size="68" />
         <button className="SearchForm__button" type="submit">
           <img className="SearchForm__button-icon" src={searchIcon} />
         </button>
