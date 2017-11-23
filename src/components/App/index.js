7c4c7ac885298d65821f243297353d4c9d16ef41
Mikhail Novikov <mixail.novikov@gmail.com>
2017-11-23 13:32:02 +0300

    add AppIntro component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import SearchForm from '../SearchForm';
 import AppLogo from '../AppLogo';
+import AppIntro from '../AppIntro';
+
 import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppLogo />
-        <SearchForm />
+        <AppLogo className="App__logo" />
+        <SearchForm className="App__search-form" />
+        <AppIntro className="App__intro" />
       </div>
     );
   }
