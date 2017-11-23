c16956db4251839ac10b08ca6aadc69e9cc5d9be
Mikhail Novikov <mixail.novikov@gmail.com>
2017-11-23 11:51:12 +0300

    reorganize components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import createStore from './redux';
 
