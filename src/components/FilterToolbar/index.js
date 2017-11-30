679f561d88690b96f72bcf3cfc0ba02618924f56
Mikhail Novikov <mixail.novikov@gmail.com>
2017-11-30 21:51:51 +0300

    delete useless wrapper component

--- src/components/FilterToolbar/index.js
+++ src/components/FilterToolbar/index.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import c from 'classnames';
 
-import FilterDropdown from './FilterDropdown';
+import {
+  filter,
+  printType,
+  sorting,
+} from '../../redux/reducers/search';
+import { Dropdown, DropdownItem } from '../Dropdown';
 
 class FilterToolbar extends Component {
   render() {
-    const { className } = this.props;
+    const {
+      filterValue, onChangeFilter,
+      printTypeValue, onChangePrintType,
+      sortingValue, onChangeSorting,
+      className,
+    } = this.props;
+
     return (
       <div className={c('FilterToolbar', className)}>
-        <FilterDropdown />
+        <Dropdown value={filterValue} defaultValue={filter.defaultValue} onChange={onChangeFilter}>
+          {filter.items.map(item => (
+            <DropdownItem key={item.key} value={item.key}>{item.value}</DropdownItem>
+          ))}
+        </Dropdown>
+
+        <Dropdown value={printTypeValue} defaultValue={printType.defaultValue} onChange={onChangePrintType}>
+          {printType.items.map(item => (
+            <DropdownItem key={item.key} value={item.key}>{item.value}</DropdownItem>
+          ))}
+        </Dropdown>
+
+        <Dropdown value={sortingValue} defaultValue={sorting.defaultValue} onChange={onChangeSorting}>
+          {sorting.items.map(item => (
+            <DropdownItem key={item.key} value={item.key}>{item.value}</DropdownItem>
+          ))}
+        </Dropdown>
       </div>
     );
   }
 }
 
-export default FilterToolbar;
+const mapStateToProps = (state) => {
+  return {
+    filterValue: filter.select(state),
+    printTypeValue: printType.select(state),
+    sortingValue: sorting.select(state),
+  }
+};
+
+const mapDispatchToProps = {
+  onChangeFilter: filter.set,
+  onChangePrintType: printType.set,
+  onChangeSorting: sorting.set,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(FilterToolbar);
