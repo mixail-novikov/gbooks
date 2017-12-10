092d87f7de41c61d308aacdb93380814d28ab47b
Mikhail Novikov <mixail.novikov@gmail.com>
2017-12-10 16:04:00 +0300

    eslint

--- src/pages/Search/components/BooksList/Book/index.jsx
+++ src/pages/Search/components/BooksList/Book/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { array, string } from 'prop-types';
+import PropTypes from 'prop-types';
 import c from 'classnames';
 import BookInfo from './BookInfo';
 
@@ -7,32 +7,46 @@ import './style.css';
 
 class Book extends Component {
   static propTypes = {
-    id: string,
-    title: string,
-    description: string,
-    authors: array,
-    year: string,
-    imageUrl: string,
-    previewLink: string,
-    link: string,
-    greenLink: string,
-    className: string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    authors: PropTypes.arrayOf(PropTypes.string),
+    year: PropTypes.string,
+    imageUrl: PropTypes.string,
+    previewLink: PropTypes.string,
+    link: PropTypes.string,
+    greenLink: PropTypes.string,
+    className: PropTypes.string,
   };
+
+  static defaultProps = {
+    title: '',
+    description: '',
+    authors: [],
+    year: '',
+    imageUrl: '',
+    previewLink: '',
+    link: '',
+    greenLink: '',
+    className: '',
+  }
 
   render() {
     const {
       title, description, imageUrl, link, greenLink, className, authors, year, previewLink,
     } = this.props;
 
+    /* eslint-disable react/no-danger */
     return (
       <div className={c('Book', className)}>
         <h2 className="Book__title">
           <a target="_blank" href={link}>{title}</a>
         </h2>
         <div className="Book__content">
-          {imageUrl && <div className="Book__image-holder">
-            <img className="Book__image" src={imageUrl} />
-          </div>}
+          {imageUrl &&
+            <div className="Book__image-holder">
+              <img alt={`${title} cover`} className="Book__image" src={imageUrl} />
+            </div>
+          }
           <div className="Book__text">
             {greenLink && <p className="Book__green-link">{greenLink}</p>}
             <BookInfo authors={authors} year={year} previewLink={previewLink} />
