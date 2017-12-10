7983b1f0556b94ba21a92bef422b07b9509ce95c
Mikhail Novikov <mixail.novikov@gmail.com>
2017-12-10 15:58:39 +0300

    eslint

--- src/pages/Search/components/BooksList/index.jsx
+++ src/pages/Search/components/BooksList/index.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { array, string } from 'prop-types';
+import PropTypes from 'prop-types';
 import c from 'classnames';
 
 import './style.css';
@@ -8,17 +8,23 @@ import './style.css';
 import Book from './Book';
 import { selectBooks } from '../../../../redux/reducers/books';
 
-class BooksList extends Component {
+class BooksList extends PureComponent {
   static propTypes = {
-    books: array,
-    className: string,
+    books: PropTypes.arrayOf(),
+    className: PropTypes.tring,
   };
 
+  static defaultProps = {
+    books: [],
+    className: '',
+  }
+
+  /* eslint-disable react/no-array-index-key */
   render() {
     const { books, className } = this.props;
     return (
       <div className={c('BooksList', className)}>
-        {books.map((book, id) => <Book className="BooksList__item" key={id} {...book} />)}
+        { books.map((book, id) => <Book className="BooksList__item" key={id} {...book} />) }
       </div>
     );
   }
