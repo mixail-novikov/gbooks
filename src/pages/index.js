cdabd682893fe75a54837ea2423b6ffa5b39b3cb
Mikhail Novikov <mixail.novikov@gmail.com>
2017-12-10 20:21:44 +0300

    eslint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router';
 
 import MainPage from './Main';
 import SearchPage from './Search';
 
-class Pages extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route path="/search" component={SearchPage} />
-      </Switch>
-    );
-  }
-}
+const Pages = () => (
+  <Switch>
+    <Route exact path="/" component={MainPage} />
+    <Route path="/search" component={SearchPage} />
+  </Switch>
+);
 
 export default Pages;
